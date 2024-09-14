cbd483388689496323a815558091fd75cd52f534
OneCoder <wushikezuo@gmail.com>
2024-09-14 22:00:08 +0800

    add log4j2 log impl

--- one-log/src/main/java/com/coderli/one/log/Slf4jLogMain.java
+++ one-log/src/main/java/com/coderli/one/log/Slf4jLogMain.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 本类用于研究Slf4j日志接口集成原理
  * @author OneCoder
  * @Blog https://www.coderli.com
  */
-public class LogMain {
+public class Slf4jLogMain {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(LogMain.class);
+        Logger logger = LoggerFactory.getLogger(Slf4jLogMain.class);
         logger.info("Hello World");
     }
 }
