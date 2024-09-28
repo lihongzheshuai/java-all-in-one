794ba264d9fb95d724d91fd7414c3480505e46d7
OneCoder <wushikezuo@gmail.com>
2024-09-28 15:10:29 +0800

    java go 14 log4j2 config and demo

--- one-log/src/main/java/com/coderli/one/log/log4j2/CPatternLogger.java
+++ one-log/src/main/java/com/coderli/one/log/log4j2/CPatternLogger.java
@@ -1,4 +1,19 @@
 package com.coderli.one.log.log4j2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 本类用于验证%c Logger Pattern
+ * @author OneCoder
+ * @Blog https://www.coderli.com
+ * @source https://github.com/lihongzheshuai/java-all-in-one
+ */
 public class CPatternLogger {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(CPatternLogger.class);
+        logger.info("%c pattern: Hello World");
+    }
+
 }
--- one-log/src/main/java/com/coderli/one/log/log4j2/CommonPatternLogger.java
+++ one-log/src/main/java/com/coderli/one/log/log4j2/CommonPatternLogger.java
@@ -1,4 +1,17 @@
 package com.coderli.one.log.log4j2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 本类用于验证Rooter Logger Pattern
+ * @author OneCoder
+ * @Blog https://www.coderli.com
+ * @source https://github.com/lihongzheshuai/java-all-in-one
+ */
 public class CommonPatternLogger {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(CommonPatternLogger.class);
+        logger.info("Common Rooter pattern: Hello World");
+    }
 }
