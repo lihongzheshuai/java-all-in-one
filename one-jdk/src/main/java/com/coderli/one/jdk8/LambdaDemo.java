3b4f617accc0fe2e2bfd39b3a16bf7653d98a9d6
OneCoder <wushikezuo@gmail.com>
2024-10-23 22:36:22 +0800

    java 8 lambde demo

--- one-jdk/src/main/java/com/coderli/one/jdk8/LambdaDemo.java
+++ one-jdk/src/main/java/com/coderli/one/jdk8/LambdaDemo.java
@@ -1,7 +1,12 @@
 package com.coderli.one.jdk8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 本来用于演练Lambda表达式
+ *
  * @blog <a href="https://www.coderli.com">https://www.coderli.com</a>
  */
 public class LambdaDemo {
@@ -14,5 +19,9 @@ public class LambdaDemo {
         LambdaInterfaceDemo lambdaInterfaceImpl = (name) -> System.out.println("LambdaInterface: " + name);
         lambdaInterfaceImpl.doSomething("Lambda Demo");
 
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David");
+        // 使用 Lambda 表达式过滤
+        List<String> filteredNames = names.stream().filter(name -> name.startsWith("A")).collect(Collectors.toList());
+        System.out.println(filteredNames);
     }
 }
